853fba529a0df83376885be829c8cf1e8a7bb45a
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-12-30 16:19:54 +0700

    A

--- definition.js
+++ definition.js
@@ -39,7 +39,7 @@ Blockly.Blocks['yolobit_update_data_weather'] = {
     Blockly.Python['yolobit_update_data_weather'] = function(block) {
         var long = block.getFieldValue('LONG')
         var lat = block.getFieldValue('LAT')
-        var location = block.getFieldValue('LOCATION')
+        var location = Blockly.Python.valueToCode(block, 'LOCATION', Blockly.Python.ORDER_ATOMIC);
         // TODO: Assemble Python into code variable.
         var code = "gc.collect()\n"+"http_response = urequests.get('https://weather1.npnlab.com/weather/getdata?code="+location+"&lon="+long+"&lat="+lat+"')\n" + "data = http_response.json().get('data')\n";
         return code;
